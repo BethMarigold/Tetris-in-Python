acae31b05ee1de67588e3cfc49de3214b0d3c779
Beth Marigold <bethmarigold36@gmail.com>
2025-06-15 19:02:44 -0700

    Added a pause button and comments to main.py

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 All of this code is from Programming With Nick's video for Tetris. There are some differences that I added though
 I added a Hard Drop and Hold Block feature
-WASD and Arrow Keys are used for movement. Hard Drop is Space. C is to Hold a block
+WASD and Arrow Keys are used for movement
+Hard Drop is Space
+C is to Hold a block
+Escape is Pause
 After 10 lines cleared, the pieces fall down faster (the game update is 50ms less then it was before)
 All credit goes to Programming With Nick for the game itself
--- main.py
+++ main.py
@@ -4,57 +4,87 @@ from colors import Colors
 
 pygame.init()
 
+# Fonts and texts
 title_font = pygame.font.Font(None, 40)
 score_surface = title_font.render("Score", True, Colors.white)
 next_surface = title_font.render("Next", True, Colors.white)
 game_over_surface = title_font.render("GAME OVER", True, Colors.white)
 hold_surface = title_font.render("Hold", True, Colors.white)
 
+# Squares that contain things
 score_rect = pygame.Rect(320, 55, 170, 60)
 next_rect = pygame.Rect(320, 215, 170, 180)
 hold_rect = pygame.Rect(320, 440, 170, 170)
 
+# Sets screen size and window name
 screen = pygame.display.set_mode((500, 620))
 pygame.display.set_caption("Python Tetris")
 
+# Gives clock the ability to be a clock
 clock = pygame.time.Clock()
 
+# Assigns the Game class from game.py to game
 game = Game()
 
+# Sets the speed and the time it updates
 GAME_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(GAME_UPDATE, 500)
 
+paused = False
+
+# Loops the game
 while True:
+    # Loops to detect events
     for event in pygame.event.get():
+        # Detect when window is closed
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        
+        # Gets key inputs
         if event.type == pygame.KEYDOWN:
+            # Ends game if it's over
             if game.game_over == True:
                 game.game_over = False
                 game.reset()
-            if event.key == pygame.K_LEFT and game.game_over == False:
+            # Moves the block left one cell
+            if paused == False and event.key == pygame.K_LEFT and game.game_over == False:
                 game.move_left()
-            if event.key == pygame.K_RIGHT and game.game_over == False:
+            # Moves the block right one cell
+            if paused == False and event.key == pygame.K_RIGHT and game.game_over == False:
                 game.move_right()
-            if event.key == pygame.K_DOWN and game.game_over == False:
+            # Brings the block down one cell
+            if paused == False and event.key == pygame.K_DOWN and game.game_over == False:
                 game.move_down()
-            if event.key == pygame.K_UP and game.game_over == False:
+            # Rotates the block
+            if paused == False and event.key == pygame.K_UP and game.game_over == False:
                 game.rotate()
-            if event.key == pygame.K_a and game.game_over == False:
+                # Moves the block left one cell
+            if paused == False and event.key == pygame.K_a and game.game_over == False:
                 game.move_left()
-            if event.key == pygame.K_d and game.game_over == False:
+            # Moves the block right one cell
+            if paused == False and event.key == pygame.K_d and game.game_over == False:
                 game.move_right()
-            if event.key == pygame.K_s and game.game_over == False:
+            # Brings the block down one cell
+            if paused == False and event.key == pygame.K_s and game.game_over == False:
                 game.move_down()
-            if event.key == pygame.K_w and game.game_over == False:
+            # Rotates the block
+            if paused == False and event.key == pygame.K_w and game.game_over == False:
                 game.rotate()
-            if event.key == pygame.K_SPACE and game.game_over == False:
+            # Drops the block as low as it can go
+            if paused == False and event.key == pygame.K_SPACE and game.game_over == False:
                 game.hard_drop()
                 game.update_score(0, 4)
-            if event.key == pygame.K_c and game.game_over == False:
+            # Holds the current block
+            if paused == False and event.key == pygame.K_c and game.game_over == False:
                 game.hold_block()
-        if event.type == GAME_UPDATE and game.game_over == False:
+            # Pauses and unpauses the game
+            if event.key == pygame.K_ESCAPE and game.game_over == False:
+                if paused == False:
+                    paused = True
+                elif paused == True:
+                    paused = False
+        if paused == False and event.type == GAME_UPDATE and game.game_over == False:
             game.move_down()
             if game.lines_cleared_total > 0 and game.lines_cleared_total % 10 == 0:
                 new_interval = max(100, 500 - (game.lines_cleared_total // 10) * 50)
@@ -84,5 +114,6 @@ while True:
     pygame.draw.rect(screen, Colors.light_blue, hold_rect, 0, 10)
     game.draw(screen)
 
+    # Updates the display and sets the game at 60fps
     pygame.display.update()
     clock.tick(60)
